c47324e96261d96e1d0b77a29352ac05ebeab327
justinzhang <121174634@qq.com>
2023-09-07 18:57:15 +0800

    兼容iOS16以下

--- Sources/Toast/SwiftUIToast.swift
+++ Sources/Toast/SwiftUIToast.swift
@@ -6,16 +6,22 @@
 //
 import SwiftUI
 
-@available(iOS 16.0, *)
-struct JCToast: UIViewRepresentable {
+@available(iOS 13.0, *)
+public struct JCToast: UIViewRepresentable {
     
-    typealias UIViewType = AppleToastView
+    public typealias UIViewType = AppleToastView
     
     @Binding var show: Bool
     var title: String
     var subTitle: String?
     
-    func makeUIView(context: Context) -> AppleToastView {
+    public init(show: Binding<Bool>, title: String, subTitle: String? = nil) {
+        _show = show
+        self.title = title
+        self.subTitle = subTitle
+    }
+    
+    public func makeUIView(context: Context) -> AppleToastView {
         let toast = Toast.text(title, subtitle: subTitle)
         let toastView = toast.view as! AppleToastView
         toastView.makeConstraintsForSwiftUI()
@@ -24,13 +30,15 @@ struct JCToast: UIViewRepresentable {
         return toastView
     }
     
-    func updateUIView(_ uiView: UIViewType, context: Context) {
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
         print("\(#function) show: \(show)")
         uiView.updateTitle(title: title, subTitle: subTitle)
+        uiView.sizeToFit()
         uiView.isHidden = !show
     }
     
-    func sizeThatFits(_ proposal: ProposedViewSize, uiView: AppleToastView, context: Context) -> CGSize? {
+    @available(iOS 16.0, *)
+    public func sizeThatFits(_ proposal: ProposedViewSize, uiView: AppleToastView, context: Context) -> CGSize? {
         print(proposal)
         var containerSize = CGSize(width: 200, height: 80)
         if
@@ -41,9 +49,10 @@ struct JCToast: UIViewRepresentable {
         let size = uiView.sizeThatFits(containerSize)
         return size
     }
+    
 }
 
-@available(iOS 16.0, *)
+@available(iOS 13.0, *)
 struct JCToast_Previews: PreviewProvider {
     
     @State static var show: Bool = true
@@ -53,12 +62,23 @@ struct JCToast_Previews: PreviewProvider {
     }
 }
 
-@available(iOS 16.0, *)
+@available(iOS 13.0, *)
 public extension View {
     
     func jcToast(isPresented: Binding<Bool>, title: String, subTitle: String? = nil) -> some View {
-        self.overlay {
-            JCToast(show: isPresented, title: title, subTitle: subTitle)
+        if #available(iOS 15.0, *) {
+            return self.overlay {
+                if #available(iOS 16.0, *) {
+                    JCToast(show: isPresented, title: title, subTitle: subTitle)
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
+        } else {
+            return ZStack {
+                self
+                JCToast(show: isPresented, title: title, subTitle: subTitle)
+            }
         }
     }
     
